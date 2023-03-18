092e90e38a74784ac3419c12533b830e313a2134
Rock070 <a0938130702@gmail.com>
2023-03-18 19:02:06 +0800

    feat: component

---
+++ src/components/Block.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <div class="block">
+    <slot />
+  </div>
+</template>
+
+<style>
+.block {
+  @apply inline-block p-30 bg-gray-500;
+}
+</style>
